cdd5682c78627e618c61cec5246e5d451008d6f0
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-12 21:55:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kasey"
+  end
+
+  get '/hometown' do
+    "My hometown is NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Broadway"
+  end
 end
